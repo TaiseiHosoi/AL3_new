6515a9c76d71efc817c37d4040d02a5d8ea686a5
LAPTOP-RL7QBGDD\taise <taiseigaming@gmail.com>
2022-04-28 15:50:07 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,88 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
+#include"DebugCamera.h"
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() { 
+	delete model_;
+	delete debugCamera_; }
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	//軸方向表示有効
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+	//ライン描画
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+}
+
+void GameScene::Update() { debugCamera_->Update(); }
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	for (int i = 0; i < 12; i++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(vertex[c_[i][0]], vertex[c_[i][1]],Vector4(1,1,1,1));
+	}
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include"DebugCamera.h"
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+  private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+	DebugCamera* debugCamera_ = nullptr;
+	uint32_t textureHandle_ = 0;
+	Model* model_ = nullptr;
+
+	WorldTransform worldTransform_;
+
+	ViewProjection viewProjection_;
+	
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+	Vector3 vertex[8] = {
+	  {0.0f, 0.0f, 0.0f},
+      {5.0f, 0.0f, 0.0f},
+      {5.0f, 0.0f, 5.0f},
+      {0.0f, 0.0f, 5.0f},
+      {0.0f, 5.0f, 0.0f},
+      {5.0f, 5.0f, 0.0f},
+      {5.0f, 5.0f, 5.0f},
+      {0.0f, 5.0f, 5.0f}
+    };
+
+	int c_[12][2] = {
+	  {0, 1},
+      {1, 2},
+      {2, 3},
+      {3, 0},
+      {0, 4},
+      {4, 5},
+	  {5, 6},
+      {6, 7},
+      {7, 4},
+      {3, 7},
+      {6, 2},
+      {1, 5}
+    };
+};
+
